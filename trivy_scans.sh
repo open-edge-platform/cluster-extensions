8c82912bcd5bc398b4fd006cc70be045b0f68358
pudelkoM <pudelkoM@users.noreply.github.com>
2025-04-04 14:07:52 -0700

    Create script to generate trivy reports

---
+++ trivy_scans.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+mkdir -p trivy_reports
+for folder in $(find ./helm -type d -print -maxdepth 1 -mindepth 1); do
+    pushd "$folder" > /dev/null
+    trivy conf -f json -o "${folder##*/}.json" ./
+    trivy scan2html generate --scan2html-flags --output "${folder##*/}.html" --from "${folder##*/}.json"
+    rm "${folder##*/}.json"
+    mv "${folder##*/}.html" ../../trivy_reports/
+    popd > /dev/null
+done
