4793a5e099b3cc110ea7aa4f0d72776027627683
pudelkoM <pudelkoM@users.noreply.github.com>
2025-04-10 16:35:24 -0700

    Add `fs` and `image` scans

--- trivy_scans.sh
+++ trivy_scans.sh
@@ -4,12 +4,25 @@
 
 set -euxo pipefail
 
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
 mkdir -p trivy_reports
 for folder in $(find ./helm -type d -print -maxdepth 1 -mindepth 1); do
     pushd "$folder" > /dev/null
-    trivy conf -f json -o "${folder##*/}.json" ./
-    trivy scan2html generate --scan2html-flags --output "${folder##*/}.html" --from "${folder##*/}.json"
-    rm "${folder##*/}.json"
-    mv "${folder##*/}.html" ../../trivy_reports/
+    trivy conf -f json -o "conf-${folder##*/}.json" ./
+    trivy scan2html generate --scan2html-flags --output "conf-${folder##*/}.html" --from "conf-${folder##*/}.json"
+    rm "conf-${folder##*/}.json"
+    mv "conf-${folder##*/}.html" ../../trivy_reports/
+    trivy fs -f json -o "fs-${folder##*/}.json" ./
+    trivy scan2html generate --scan2html-flags --output "fs-${folder##*/}.html" --from "fs-${folder##*/}.json"
+    rm "fs-${folder##*/}.json"
+    mv "fs-${folder##*/}.html" ../../trivy_reports/
     popd > /dev/null
+done
+
+for image in "edgedns-coredns" "kubevirt-helper" "intel-gpu-debug"; do
+    trivy image -f json -o "image-${image##*/}.json" "${image}:$BRANCH"
+    trivy scan2html generate --scan2html-flags --output "image-${image##*/}.html" --from "image-${image##*/}.json"
+    rm "image-${image##*/}.json"
+    mv "image-${image##*/}.html" trivy_reports/
 done
